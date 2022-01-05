cb4d9446a278ac3aa5fa451a54c61894f3e2d3d2
320Jackson <35259118+320Jackson@users.noreply.github.com>
2022-01-06 00:51:33 +0800

    Delete Week5 directory

--- Week5/test/ut_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-#include "./ut_gcd.h"
-#include "./ut_fraction.h"
-
-int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
